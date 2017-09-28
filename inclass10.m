c32ab6c95a87a98202904df38b8417566803460e
warmflasha <warmflasha@gmail.com>
2017-09-28 13:41:51 -0500

    updated

--- inclass10.m
+++ inclass10.m
@@ -1,5 +1,5 @@
 %In this directory, you will find some example data, which is a 300x8
-%array stored in a .mat file. Imagine these are measurements for 300 genes are 8 different time
+%array stored in a .mat file. Imagine these are measurements for 300 genes at 8 different time
 %points
 
 %1. Use matlab to perform pca on this data, getting the transformation
@@ -9,7 +9,7 @@
 %the data? 
 
 %3. Using kmeans clustering to cluster the data into 3 clusters. 
-% Make a plot of the first coordinate of the raw data vs the second color coded
+% Make a plot of the first coordinate of the raw data vs the second, color coded
 % by which cluster it is in. Make the same plot but use the first two
 % principle components instead. 
 
